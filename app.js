02fb33c3de238cd06b5537fc725fa83a0e955622
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-03-09 00:54:31 +0530

    adding files

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var express       = require("express"),
     bodyParser    = require("body-parser"),
     mongoose      = require("mongoose"),
     passport      = require("passport"),
+    flash         = require("connect-flash"),
     LocalStrategy = require("passport-local"),
-    methodOverride= require("method-override");
+    Campground    = require("./models/campgrounds"),
+    Comment       = require("./models/comment"),
+    methodOverride= require("method-override"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds");
 
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/captioner",{ useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
 
+// PASSPORT CONFIGURATION
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
@@ -20,9 +31,21 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
 
 app.listen(8080, process.env.IP, function(){
    console.log("The Captionator Server Has Started!");
